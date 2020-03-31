f79a74babb431c5e8c988d7fe4f62ff5c3a90982
Oscar <oscar.ramos@ucsp.edu.pe>
2020-03-30 19:21:54 -0500

    Better styles + AddNote prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from 'react'
+import { Icon } from 'semantic-ui-react';
+
 import Note from "./components/note/note.component";
 
-const App = () => (
-  <div>
-    <Note text="This is a note 1"/>
-    <Note text="This is a note 2"/>
-    <Note text="This is a note 3"/>
-  </div>
-)
+import './App.css'
 
-export default App
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          title: 'Title',
+          text: 'This is a note 11'
+        },
+        {
+          title: 'Title',
+          text: 'This is a note 22'
+        },
+        {
+          title: 'Title',
+          text: 'This is a note 33'
+        }
+      ]
+    };
+  }
+
+  addIcon = () => {
+    const newNote = {
+      title: 'New title',
+      text: 'New text'
+    }
+
+    this.setState({
+      notes: [...this.state.notes, newNote]
+    });
+  }
+
+
+  render() {
+    return (
+      <div className='note-page'>
+        <Icon name='plus square outline' size='big' onClick={this.addIcon} />
+        {
+          this.state.notes.map(({ title, text }) => (
+            <Note title={title} text={text}/>
+          ))
+        }
+      </div>
+    );
+  }
+
+}
+
+export default App;
--- src/components/note/note.component.jsx
+++ src/components/note/note.component.jsx
@@ -3,17 +3,20 @@ import { Card } from "semantic-ui-react";
 import 'semantic-ui-css/semantic.min.css'
 import './note.styles.scss'
 
-const Note = props => (
-  <div className='note'>
-    <Card>
-      <Card.Content>
-        <Card.Header>Note</Card.Header>
-        <Card.Description>
-          {props.text}
-        </Card.Description>
-      </Card.Content>
-    </Card>
-  </div>
-);
+const Note = props => {
+  const { title, text } = props;
+  return (
+    <div className='note'>
+      <Card className='card'>
+        <Card.Content className='card-content'>
+          <Card.Header className='card-header'>{title}</Card.Header>
+          <Card.Description className='card-description'>
+            {text}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default Note;
