5799de2eff70db45df9862532c17dbe5ce299cd8
Oscar <oscar.ramos@ucsp.edu.pe>
2020-03-30 22:30:46 -0500

    Added Remove Note

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Icon } from 'semantic-ui-react';
 
 import Note from "./components/note/note.component";
 
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
+
 
 class App extends React.Component {
   constructor(props) {
@@ -11,14 +13,17 @@ class App extends React.Component {
     this.state = {
       notes: [
         {
+          id: 1,
           title: 'Title',
           text: 'This is a note 11'
         },
         {
+          id: 2,
           title: 'Title',
           text: 'This is a note 22'
         },
         {
+          id: 3,
           title: 'Title',
           text: 'This is a note 33'
         }
@@ -26,10 +31,12 @@ class App extends React.Component {
     };
   }
 
-  addIcon = () => {
+  addNote = () => {
+    const lastid = this.state.notes.length ? this.state.notes[this.state.notes.length - 1].id : 1;
     const newNote = {
-      title: 'New title',
-      text: 'New text'
+      id: lastid + 1,
+      title: 'Title',
+      text: 'Text'
     }
 
     this.setState({
@@ -37,14 +44,20 @@ class App extends React.Component {
     });
   }
 
+  removeNote = toRemoveNote => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== toRemoveNote.id)
+    });
+  }
+
 
   render() {
     return (
       <div className='note-page'>
-        <Icon name='plus square outline' size='big' onClick={this.addIcon} />
+        <Icon name='plus square outline' size='big' onClick={this.addNote} className='clickable'/>
         {
-          this.state.notes.map(({ title, text }) => (
-            <Note title={title} text={text}/>
+          this.state.notes.map(note => (
+            <Note key={note.id} note={note} removeNote={this.removeNote}/>
           ))
         }
       </div>
--- src/components/note/note.component.jsx
+++ src/components/note/note.component.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Card } from "semantic-ui-react";
+import { Card, Icon } from "semantic-ui-react";
+
 import 'semantic-ui-css/semantic.min.css'
 import './note.styles.scss'
 
-const Note = props => {
-  const { title, text } = props;
-  return (
+const Note = ({ note, removeNote }) => (
     <div className='note'>
       <Card className='card'>
-        <Card.Content className='card-content'>
-          <Card.Header className='card-header'>{title}</Card.Header>
-          <Card.Description className='card-description'>
-            {text}
+        <Card.Content>
+          <Card.Header>
+            <div className='card-header'>
+              <span>{note.title}</span>
+              <Icon name='remove' className='clickable' onClick={() => removeNote(note)}/>
+            </div>
+          </Card.Header>
+          <Card.Description>
+            <span>
+              {note.text}
+            </span>
           </Card.Description>
         </Card.Content>
       </Card>
     </div>
-  );
-};
+  )
+
 
 export default Note;
