fc17f3c6e28a2945a1163a9befd0cd1cee4a7e0e
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-18 12:08:19 -0500

    Create list of cards

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ import NoteCard from './components/NoteCard/NoteCard'
 import Grid from '@material-ui/core/Grid'
 
 function App() {
-  const [title, setTitle] = useState('My title')
-  const [creationDate, setCreationDate] = useState('17th JAN')
-  const [content, setContent] = useState('ajaja')
+  const [cards, setCards] = useState([
+    {
+      title: 'My title',
+      creationDate: '17 jan',
+      content: 'my content',
+      backgroundColor: '#FFB507'
+    },
+    {
+      title: 'My title 2',
+      creationDate: '20 jan',
+      content: 'My second content',
+      backgroundColor: '#1FD0BF'
+    },
+  ])
 
   return (
     <Container maxWidth='xs'>
       <Grid container spacing={1} direction='column'>
-        <Grid item>
-          <NoteCard title={title}
-                    creationDate={creationDate}
-                    content={content}
-                    backgroundColor="#FFB507"
-          />
-        </Grid>
-        <Grid item>
-          <NoteCard title={title}
-                    creationDate={creationDate}
-                    content={content}
-                    backgroundColor='blue'
-          />
-        </Grid>
+        {
+          cards.map(card => (
+            <Grid item>
+              <NoteCard title={card.title}
+                        creationDate={card.creationDate}
+                        content={card.content}
+                        backgroundColor={card.backgroundColor}
+              />
+            </Grid>
+          ))
+        }
       </Grid>
     </Container>
   )
